7cda1c8a364e28891fdfa7b36abf877ab39e7691
Bartosz <bartoszbanach199@gmail.com>
2024-11-12 16:14:01 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,30 @@
+﻿namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    public int Size { get; }
+    private Rectangle boundaries;
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size >20)
+        {
+            throw new ArgumentOutOfRangeException("Rozmiar musi być między 5 a 20");
+        }
+        Size = size;
+        boundaries = new Rectangle(0, 0, size - 1, size - 1);
+    }
+    public override bool Exist(Point p)
+    {
+        return boundaries.contains(p);
+    }
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+        return Exist(nextPoint) ? nextPoint : p;
+    }
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextPoint = p.NextDiagonal(d);
+        return Exist(nextPoint) ? nextPoint : p;
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Simulator.Creature;
+﻿using Simulator.Maps;
+using static Simulator.Creature;
 
 namespace Simulator;
 
@@ -10,6 +11,7 @@ internal class Program
         Console.WriteLine(p.Next(Direction.Right));          // (11, 25)
         Console.WriteLine(p.NextDiagonal(Direction.Right));  // (11, 24)
         Lab5a();
+        Lab5b();
     }
     static void Lab4a()
     {
@@ -82,7 +84,46 @@ internal class Program
         Console.WriteLine($"Punkt {pointInside} w prostokącie: {rect.contains(pointInside)}");
         Console.WriteLine($"Punkt {pointOutside} w prostokącie: {rect.contains(pointOutside)}");
     }
+    static void Lab5b()
+    {
+        Console.WriteLine("TEST MAP\n");
+        try
+        {
+            var badmap = new SmallSquareMap(4);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine($"Wykryto błąd: {ex.Message}");
+        }
+        var map = new SmallSquareMap(10);
+        var pointInside = new Point(5, 5);
+        var pointOutside = new Point(15, 15);
+        Console.WriteLine($"Mapa ma rozmiar: {map.Size}");
+        Console.WriteLine($"Punkt {pointInside} jest na mapie: {map.Exist(pointInside)}"); // powinno być true
+        Console.WriteLine($"Punkt {pointOutside} jest na mapie: {map.Exist(pointOutside)}"); // powinno być false
+        var nextPoint = map.Next(pointInside, Direction.Right);
+        Console.WriteLine($"Nastepny punkt od {pointInside} na prawo: {nextPoint}");
+        var nextPoint2 = map.Next(nextPoint, Direction.Up);
+        Console.WriteLine($"Nastepny punkt od {nextPoint} w górę: {nextPoint2}");
+        var nextPoint3 = map.Next(nextPoint2, Direction.Left);
+        Console.WriteLine($"Nastepny punkt od {nextPoint2} na lewo: {nextPoint3}");
+        var nextPoint4 = map.Next(nextPoint3, Direction.Down);
+        Console.WriteLine($"Nastepny punkt od {nextPoint3} w dół: {nextPoint4}");
+        var nextDiagonalPoint = map.NextDiagonal(pointInside, Direction.Right);
+        Console.WriteLine($"Nastepny punnkt na skos od {pointInside} na prawo: {nextDiagonalPoint}");
+        var nextDiagonalPoint2 = map.NextDiagonal(nextDiagonalPoint, Direction.Up);
+        Console.WriteLine($"Nastepny punnkt na skos od {nextDiagonalPoint} w górę: {nextDiagonalPoint2}");
+        var nextDiagonalPoint3 = map.NextDiagonal(nextDiagonalPoint2, Direction.Left);
+        Console.WriteLine($"Nastepny punnkt na skos od {nextDiagonalPoint2} na lewo: {nextDiagonalPoint3}");
+        var nextDiagonalPoint4 = map.NextDiagonal(nextDiagonalPoint3, Direction.Down);
+        Console.WriteLine($"Nastepny punnkt na skos od {nextDiagonalPoint3} w dół: {nextDiagonalPoint4}");
+        var borderPoint = new Point(map.Size -1, map.Size-1);
+        var nextPoint5 = map.Next(borderPoint, Direction.Right);
+        Console.WriteLine($"Nastepny punkt od {borderPoint} na prawo: {nextPoint5}"); //powinno zwrócić ten sam punkt
+
+    }
 }
+
 
 
 
